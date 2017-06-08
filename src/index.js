544e86e6978c8622f50fcb6e2f4912e592672ae0
Up Chen <wallat@gmail.com>
2017-06-08 16:40:02 +0800

    Init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import './index.css'
+import '../semantic/dist/semantic.min.css';
+
+render(
+    <App />,
+	document.getElementById('root')
+)
